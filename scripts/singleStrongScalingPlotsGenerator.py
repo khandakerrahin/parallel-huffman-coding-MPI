d12a57606ab4772a09c799078ef067280994bf27
Shaker Mahmud Khandaker <shaker@khandakerrahin.com>
2023-02-07 11:16:13 +0100

    updated scripts and results

--- scripts/singleStrongScalingPlotsGenerator.py
+++ scripts/singleStrongScalingPlotsGenerator.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 num_procs = [1, 2, 4, 8, 16, 32, 64, 128]
 
 # for file size 6.5MB
-times = [3.941, 2.082, 1.223, 0.861, 2.123, 1.971, 3.263, 4.476]
-
+# without resource request
+#times = [3.941, 2.082, 1.223, 0.861, 2.123, 1.971, 3.263, 4.476]
+# with resource request
+times = [3.941, 2.174, 1.0870, 1.135, 0.874, 1.317, 1.263, 1.476]
 # Plot the results
 plt.plot(num_procs, times, '-o', label='Result scaling')
 plt.xscale('log', base=2)
